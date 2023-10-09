e0669f5b1b4300b3ec0c49e29a6328bc9e8d98b6
Hocwin <146694692+Hocwin@users.noreply.github.com>
2023-10-09 13:10:36 +0700

    LanjutanWeb

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route ke profile
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+// route dengan parameter (wajib)
+Route::get('/mahasiswa/{nama}', function ($nama = "Hocwin") {
+    echo "<h1>Halo Nama saya $nama</h2>";
+});
+
+// route dengan parameter (tidak wjb)
+Route::get('/mahasiswa/{nama?}', function ($nama = "Hocwin") {
+    echo "<h1>Halo Nama saya $nama</h2>";
+});
+
+// route dengan parameter lbh dari 1
+Route::get('/profil/{nama}/{pekerjaan?}', function ($nama = "Hocwin", $pekerjaan = "Mahasiswa") {
+    echo "<h1>Halo Nama saya $nama dan saya adalah $pekerjaan</h1>";
+});
+
+// Redirect
+Route::get('/hubungi', function () {
+    echo "<h1>Hubungi kami</h1>";
+})->name("call"); //named route
+
+Route::redirect("/contact", "/hubungi");
+
+Route::get('/halo', function () {
+    echo "<a href = '". route('call'). "'>" . route('call'). "</a>";
+});
+
+// Route Group
+Route::prefix("/dosen")->group(function () {
+    Route::get("/jadwal", function(){
+        echo "<h1>Jadwwal Dosen</h1>";
+    });
+    Route::get("/materi", function(){
+        echo "<h1>Materi Perkuliahan</h1>";
+    });
+});
